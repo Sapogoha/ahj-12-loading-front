507f60fdd1082edcdf166ebfb541b4f178922c76
Aleksey Sokolov <sokolov.alexey@me.com>
2022-06-14 15:11:49 +0300

    sw updated

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,16 @@ import News from './News';
 const news = new News(document.querySelector('.news'));
 news.init();
 
-(async () => {
-  try {
-    if (navigator.serviceWorker) {
-      await navigator.serviceWorker.register('/service.worker.js');
-      console.log('sw registered');
-    }
-  } catch (e) {
-    console.log(e);
-  }
-})();
+// (async () => {
+//   try {
+//     if (navigator.serviceWorker) {
+//       await navigator.serviceWorker.register('/service.worker.js');
+//       console.log('sw registered');
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// })();
 
 // if (navigator.serviceWorker) {
 //   window.addEventListener('load', async () => {
