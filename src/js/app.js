1775141ddfc0d210948468e724192d3e68752a35
Aleksey Sokolov <sokolov.alexey@me.com>
2022-06-14 15:25:29 +0300

    sw path

--- src/js/app.js
+++ src/js/app.js
@@ -3,26 +3,26 @@ import News from './News';
 const news = new News(document.querySelector('.news'));
 news.init();
 
-// (async () => {
-//   try {
-//     if (navigator.serviceWorker) {
-//       await navigator.serviceWorker.register('/service.worker.js');
-//       console.log('sw registered');
-//     }
-//   } catch (e) {
-//     console.log(e);
-//   }
-// })();
-
-if (navigator.serviceWorker) {
-  window.addEventListener('load', async () => {
-    try {
-      if (navigator.serviceWorker) {
-        await navigator.serviceWorker.register('./service.worker.js');
-        console.log('sw registered');
-      }
-    } catch (error) {
-      console.log(error);
+(async () => {
+  try {
+    if (navigator.serviceWorker) {
+      await navigator.serviceWorker.register('/service.worker.js');
+      console.log('sw registered');
     }
-  });
-}
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
+// if (navigator.serviceWorker) {
+//   window.addEventListener('load', async () => {
+//     try {
+//       if (navigator.serviceWorker) {
+//         await navigator.serviceWorker.register('./service.worker.js');
+//         console.log('sw registered');
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+// }
