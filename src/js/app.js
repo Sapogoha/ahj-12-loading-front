cdd04b4a00419cab9ce9b5b1157016db2255e42a
Aleksey Sokolov <sokolov.alexey@me.com>
2022-06-16 11:18:50 +0300

    sw

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,22 @@ import News from './News';
 const news = new News(document.querySelector('.news'));
 news.init();
 
-(async () => {
-  try {
-    if (navigator.serviceWorker) {
-      await navigator.serviceWorker.register('./service.worker.js');
-      console.log('sw registered');
-    }
-  } catch (e) {
-    console.log(e);
-  }
-})();
+if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
+  navigator.serviceWorker.register('/sw.js').catch((error) => {
+    console.log('Service worker registration failed:', error);
+  });
+}
+
+// (async () => {
+//   try {
+//     if (navigator.serviceWorker) {
+//       await navigator.serviceWorker.register('./service.worker.js');
+//       console.log('sw registered');
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// })();
 
 // if (navigator.serviceWorker) {
 //   window.addEventListener('load', async () => {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,19 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 // const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   optimization: {
     minimizer: [`...`, new CssMinimizerPlugin()],
   },
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      cleanupOutdatedCaches: true,
+    }),
+  ],
 });
